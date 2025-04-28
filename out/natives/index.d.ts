cbb48fffd0a579881300a445e23cc03349b8c1e4
RsMan-Dev <contact@rsman.fr>
2025-04-28 23:35:26 +0200

    export hooks

--- out/natives/index.d.ts
+++ out/natives/index.d.ts
@@ -2,4 +2,5 @@ import createInstance from "./createInstance";
 export * from "./flow";
 export * from "./rendering";
 export * from "./createInstance";
+export * from "./hooks";
 export default createInstance;
--- src/natives/index.ts
+++ src/natives/index.ts
@@ -3,5 +3,6 @@ import createInstance from "./createInstance"
 export * from "./flow"
 export * from "./rendering"
 export * from "./createInstance"
+export * from "./hooks"
 
 export default createInstance;
