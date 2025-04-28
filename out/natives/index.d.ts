ad62827ea3eb241bab55361317c3e3b98b229394
RsMan-Dev <contact@rsman.fr>
2025-04-28 23:27:05 +0200

    add instance

--- out/natives/index.d.ts
+++ out/natives/index.d.ts
@@ -1,4 +1,5 @@
 import createInstance from "./createInstance";
 export * from "./flow";
 export * from "./rendering";
+export * from "./createInstance";
 export default createInstance;
--- src/natives/index.ts
+++ src/natives/index.ts
@@ -2,5 +2,6 @@ import createInstance from "./createInstance"
 
 export * from "./flow"
 export * from "./rendering"
+export * from "./createInstance"
 
 export default createInstance;
