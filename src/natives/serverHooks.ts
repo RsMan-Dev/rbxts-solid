457afa4ea979e41ff3bec5da0a65e231a9c4d446
RsMan-Dev <contact@rsman.fr>
2025-04-28 23:51:08 +0200

    profile store async import

--- src/natives/serverHooks.ts
+++ src/natives/serverHooks.ts
@@ -1,24 +1,29 @@
-import ProfileStore from "@rbxts/profile-store";
-import { Players } from "@rbxts/services";
-import { clearTimeout, setTimeout, TimeoutSymbol } from "@rbxts/jsnatives";
+import { Players, RunService } from "@rbxts/services";
+import { clearTimeout, Object, setTimeout, TimeoutSymbol } from "@rbxts/jsnatives";
 import { createEffect, onCleanup, createMutable, withoutWrap } from "@rbxts/signals";
-
+import type { Profile } from "@rbxts/profile-store";
 
 export function useStore<T extends object>(defaults = {} as T, uniqueKey = "", player?: Player, eraseAtInit = false) {
-  const profileStore = ProfileStore.New("PlayerData", defaults)
-  const session = profileStore.StartSessionAsync(`player_${player?.UserId ?? "server"}_${uniqueKey}`, {
-    Cancel: () => player !== undefined ? player.Parent !== Players : false,
-  })
-  if (player !== undefined) {
-    session.AddUserId(player.UserId)
-    session.Reconcile()
-  }
+  if (RunService.IsClient()) throw "this hook is server only"
 
-  const value = createMutable({ data: session.Data })
+  let value = createMutable({ data: Object.dup(defaults, true) })
+  let session: Profile<T, object> | undefined
+
+  import("@rbxts/profile-store").then(({ default: ProfileStore }) => {
+    const profileStore = ProfileStore.New("PlayerData", defaults)
+    session = profileStore.StartSessionAsync(`player_${player?.UserId ?? "server"}_${uniqueKey}`, {
+      Cancel: () => player !== undefined ? player.Parent !== Players : false,
+    })
+    if (player !== undefined) {
+      session!.AddUserId(player.UserId)
+      session!.Reconcile()
+    }
+  })
 
   if (eraseAtInit) value.data = defaults // DEV OLNY, ERASES DATA
 
   function save(unwrapped: T) {
+    if (!session) return
     session.Data = unwrapped
     session.Save()
   }
